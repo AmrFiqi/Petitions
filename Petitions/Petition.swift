8f4d86f81c9a685b946a2317518e79ad78951a26
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-01-11 22:00:14 +0200

    Initial Commit

---
+++ Petitions/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Petitions
+//
+//  Created by Amr El-Fiqi on 11/01/2023.
+//
+
+import Foundation
+
+struct Petition: Codable{
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
---
+++ Petitions/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Petitions
+//
+//  Created by Amr El-Fiqi on 11/01/2023.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    var results: [Petition]
+}
